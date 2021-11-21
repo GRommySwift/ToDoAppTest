745e7e5dbb071e3d279363e0452b4204b0057ae3
Roman <romaholovai@gmail.com>
2021-11-21 16:20:42 +0100

    Initial Commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Roman Holovai on 21.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
